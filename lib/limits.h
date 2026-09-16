732ade97c4a1f4b2aa4a6e73472eb68e32d5803d
alessioMaroni <alessiomaroni0@gmail.com>
2026-09-16 18:19:39 +0200

    first commit, integer limits (int)

---
+++ lib/limits.h
@@ -0,0 +1,12 @@
+#ifndef LIMITS_H
+#define LIMITS_H
+
+#define INT_MAX (int)(~0U >> 1)
+
+#define INT_MIN ~INT_MAX
+
+#define UINT_MAX ~0U
+
+#define UINT_MIN 0U
+
+#endif
---
+++ test.c
@@ -0,0 +1,11 @@
+#include "lib/limits.h"
+
+#include <stdio.h>
+
+void main(void) 
+ {
+    printf("INT MAX: %d\n", INT_MAX);
+    printf("INT MIN: %d\n", INT_MIN);
+    printf("UINT MAX: %u\n", UINT_MAX);
+    printf("UNIT MIN: %u\n", UINT_MIN);
+ }
